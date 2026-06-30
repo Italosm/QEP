a594c56da051006ee53e328df7cd15f43898b70e
Italo Souza Melo <italosm29@gmail.com>
2026-06-30 11:35:46 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Força a Vercel a empacotar o arquivo do SQLite no servidor
+  outputFileTracingIncludes: {
+    "/*": ["./prisma/**/*"],
+  },
+
+  // 2. Garante que o Prisma funcione perfeitamente no ambiente Serverless
+  serverExternalPackages: ["@prisma/client", "prisma"],
+
+  // 3. Suas configurações de imagens que já estavam certas
   images: {
     remotePatterns: [
       {
